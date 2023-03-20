cd7ea56fb2b08bf59720c053c66ce09f04818f96
tractortoby <tobychung@duck.com>
2023-03-20 22:15:37 +0800

    feat: remove all thr URL parameters after the '?'

--- no-more-more-tweets.js
+++ no-more-more-tweets.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         No more "More Tweets" by adding question mark
 // @namespace    https://twitter.com/runningtractor
-// @version      0.2
+// @version      0.3
 // @description  By adding a question mark to the end of a Twitter URL, you can avoid seeing the "More Tweets" message.
 // @author       https://twitter.com/runningtractor
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
@@ -10,7 +10,15 @@
 // @match        *://twitter.com/*
 // ==/UserScript==
 
+const questionMarkIndex = window.location.href.indexOf('?');
+
+if (window.location.href.length > questionMarkIndex + 1) {
+  // Remove all the URL parameters after the "?"
+  window.location.href = window.location.href.slice(0, questionMarkIndex + 1);
+}
+
 const regex = /\/status\/\d+$/;
+
 if (regex.test(window.location.href)) {
   if (window.location.href.slice(-1) !== '?') {
     window.location.href += '?';
